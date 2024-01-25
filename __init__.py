94b18f5f635db5f6ab1225491211674efbc7f5d5
shadowrider <plugins@fs-plugins.de>
2024-01-25 09:00:55 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,57 @@
-# -*- coding: utf-8 -*-
 from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
 from os import environ as os_environ
 import gettext
+
+my_version="9.70"
+dat_dir = "/etc/ConfFS/"
+defconf = {
+			"version":"",
+			"timestartstandby":"No",
+			"startscreen_plus":"True",
+			"timer_on":"On",
+			"ansicht":1,
+			"akt_intv":0,
+			"m_dauer":0,
+			"m_sound":"None",
+			"m_sound_vol":"10,100",
+			"autosync":"No",
+			"cal_menu":1,
+			"adr_menu":1,
+			"adr_on":1,
+			"startanzeige2":"systemstart",
+			"start_display_autohide":0,
+			"vorschaum":1,
+			"kalender_art":"Gregorian",
+			"holidays_in_startscreen":"Yes",
+			"doubles_in_startscreen":"All",
+			"starttime":"None",
+			"altloesch_on":"No",
+			"altloesch":365,
+			"last Backup Path" :"/hdd/",
+			"sec_file":"none",
+			"extern_color":"On",
+			"online_on_kal":1,
+			"erinn_ext":1,
+			"l4l_on":"No",
+			"l4l_lcd":1,
+			"l4l_screen":2,
+			"l4l_font":40,
+			"l4lm_font":60,
+			"l4l_ges_file":"On",
+			"l4l_ges_file_len":50,
+			"m_radio_url":"",
+			"schicht_send_url":"",
+			"cals_dir":'',
+			"ferien":0,
+			"doubles":0,
+			"l_ferien":0,
+			"start_back":"no_l",
+			"bgr_skin":1,
+			"schicht_art":"0,0,0,Schicht",
+			"schicht_colors":{"F":"#008B45","S":"#FFD700","N":"#3A5FCD","fr":"#858585"},
+			"z_liste": "0,1,0,1,1,0,0,0,0,0",
+		}
 
 def localeInit():
 	lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
@@ -12,7 +61,8 @@ def localeInit():
 def _(txt):
 	t = gettext.dgettext("PlanerFS", txt)
 	if t == txt:
-		print "[PlanerFS] fallback to default translation for", txt
+		#print "[PlanerFS] fallback to default translation for", txt
+		print("[PlanerFS] fallback to default translation for %s" % txt)
 		t = gettext.gettext(txt)
 	return t
 
